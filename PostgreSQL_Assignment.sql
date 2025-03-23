a134ad8ab11dd8249835abacffa03279c93021c5
RittikaDev <rittika.dev@gmail.com>
2025-03-23 19:35:58 +0600

    feat: create tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,32 @@
 -- DATABASE SETUP
 
 CREATE DATABASE bookstore_db;
+
+---------- TABLE CREATIONS ----------
+
+--  "books" TABLE
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    price DECIMAL CHECK (price >= 0),
+    stock INT CHECK (stock >= 0),
+    published_year INT
+);
+
+-- "customers" TABLE
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
+);
+
+-- "orders" TABLE
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    book_id INT REFERENCES books(id),
+    quantity INT CHECK (quantity > 0),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
